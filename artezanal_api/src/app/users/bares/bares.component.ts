06b8488f8336c8dc419d7026ef2cd1ef7eb0c463
josueskt <josueskt32@gmail.com>
2023-06-10 15:06:56 -0500

    asdsadas

---
+++ artezanal_api/src/app/users/bares/bares.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bares',
+  templateUrl: './bares.component.html',
+  styleUrls: ['./bares.component.css']
+})
+export class BaresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

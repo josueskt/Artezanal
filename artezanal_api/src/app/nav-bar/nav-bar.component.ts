307fdec3a30cb169a0ec00fff9857be1ca845b7a
josueskt <josueskt32@gmail.com>
2023-06-19 15:38:49 -0500

    nav_menu

---
+++ artezanal_api/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent {
+ user_image ="https://images.immediate.co.uk/production/volatile/sites/4/2023/02/Midjourney-small-f3a9034.jpg?quality=90&resize=940,400";
+ user_name = "josue skt";
+
+
+
+
+}

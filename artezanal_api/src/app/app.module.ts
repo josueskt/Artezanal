c6c8d753e4d2f25d3bab601426d974d5b999fd04
josueskt <josueskt32@gmail.com>
2023-06-19 19:37:26 -0500

    asdasd

--- artezanal_api/src/app/app.module.ts
+++ artezanal_api/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
+import { ROUTES, RouterModule, Routes } from '@angular/router';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { FormsModule } from '@angular/forms';
 
+const rutas:Routes=[
+  {path: '/', component: HomeComponent},
+  {path: '/nosotros', component: NosotrosComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    NosotrosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

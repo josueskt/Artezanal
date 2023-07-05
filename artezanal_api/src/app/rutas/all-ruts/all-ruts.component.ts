5628e82cdc166cc60c8f1759afdaf206263340c7
A1509 <erazoalexis16@gmail.com>
2023-07-04 22:56:57 -0500

    actualizacion

--- artezanal_api/src/app/rutas/all-ruts/all-ruts.component.ts
+++ artezanal_api/src/app/rutas/all-ruts/all-ruts.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Home_serviciosService } from '../../home/home_servicios.service';
 
 @Component({
   selector: 'app-all-ruts',
   templateUrl: './all-ruts.component.html',
   styleUrls: ['./all-ruts.component.css']
-})
-export class AllRutsComponent {
 
-}
+})
+export class AllRutsComponent implements OnInit{
+    nombreUsuario: string;
+    constructor(private rutas: Home_serviciosService ) {
+
+    }
+    games:any
+
+    ngOnInit() {
+
+      this.rutas.getRutas().subscribe(
+
+        res => {
+          this.games =res
+          console.log(this.games)
+        },
+        err => console.log(err),
+
+      );
+
+    }
+
+  }
+
